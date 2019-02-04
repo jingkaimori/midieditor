09aaebb3753eccb742cab4ca872ca5151e76a0e6
PROPHESSOR <xenteze@gmail.com>
2019-02-05 00:49:23 +0200

    Rebranding build scripts

--- packaging/windows/create_windows_setup.sh
+++ packaging/windows/create_windows_setup.sh
@@ -7,38 +7,38 @@
 # MIDIEDITOR_RELEASE_VERSION_ID=2
 # MIDIEDITOR_RELEASE_VERSION_STRING=3.1.0
 # MIDIEDITOR_PACKAGE_VERSION=1
-# MIDIEDITOR_BINARY_WINDOWS=relative/path/to/MidiEditor.exe
+# MIDIEDITOR_BINARY_WINDOWS=relative/path/to/ProMidEdit.exe
 # INSTALLJAMMER=/path/to/installjammer
 #
 
 # Setup folder structure
-mkdir MidiEditor-win32
-mkdir MidiEditor-win32/win_root/
+mkdir ProMidEdit-win32
+mkdir ProMidEdit-win32/win_root/
 
 # Copy binary
-cp $MIDIEDITOR_BINARY_WINDOWS MidiEditor-win32/win_root/MidiEditor.exe
+cp $MIDIEDITOR_BINARY_WINDOWS ProMidEdit-win32/win_root/ProMidEdit.exe
 
 # Copy assistant files
-cp -R packaging/windows/assistant MidiEditor-win32/win_root/assistant
+cp -R packaging/windows/assistant ProMidEdit-win32/win_root/assistant
 
 # Copy metronome
-cp -R packaging/metronome MidiEditor-win32/win_root/metronome
+cp -R packaging/metronome ProMidEdit-win32/win_root/metronome
 
 # Create manual
-cp -R midieditor-manual/. MidiEditor-win32/win_root/assistant
-cp -a packaging/manual/. MidiEditor-win32/win_root/assistant
+cp -R promidedit-manual/. ProMidEdit-win32/win_root/assistant
+cp -a packaging/manual/. ProMidEdit-win32/win_root/assistant
 D=${PWD}
-cd MidiEditor-win32/win_root/assistant
-qcollectiongenerator midieditor-collection.qhcp -o midieditor-collection.qhc
+cd ProMidEdit-win32/win_root/assistant
+qcollectiongenerator promidedit-collection.qhcp -o promidedit-collection.qhc
 cd $D
 
-cp -R packaging/windows/windows-installer/. MidiEditor-win32
+cp -R packaging/windows/windows-installer/. ProMidEdit-win32
 
-cd MidiEditor-win32
+cd ProMidEdit-win32
 
 sh ../$INSTALLJAMMER -DVersion $MIDIEDITOR_RELEASE_VERSION_STRING --build-for-release windows-installer.mpi
 
 cd ..
 
 mkdir releases
-cp -a MidiEditor-win32/output/MidiEditor-$MIDIEDITOR_RELEASE_VERSION_STRING-Setup.exe releases/MidiEditor-$MIDIEDITOR_RELEASE_VERSION_STRING-Setup.exe
+cp -a ProMidEdit-win32/output/ProMidEdit-$MIDIEDITOR_RELEASE_VERSION_STRING-Setup.exe releases/ProMidEdit-$MIDIEDITOR_RELEASE_VERSION_STRING-Setup.exe
