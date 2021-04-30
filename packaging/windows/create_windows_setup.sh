f96f806fe2a476b6c93846b704db10ba6171028a
Dustin Hopper <hopper.dustin@gmail.com>
2021-04-29 20:27:19 -0600

    packaging: Necessary fixes to package releases (at least locally, it works)

--- packaging/windows/create_windows_setup.sh
+++ packaging/windows/create_windows_setup.sh
@@ -11,23 +11,28 @@
 # INSTALLJAMMER=/path/to/installjammer
 #
 
+# MIDIEDITOR_RELEASE_VERSION_ID=3 MIDIEDITOR_RELEASE_VERSION_STRING=4.0.0 MIDIEDITOR_PACKAGE_VERSION=1 MIDIEDITOR_BINARY_WINDOWS=/d/build-midieditor-Desktop_Qt_5_15_2_MSVC2019_64bit-Release/bin/MidiEditor.exe INSTALLJAMMER="/c/Program Files (x86)/InstallJammer/installjammer" ./create_windows_setup.sh
+
 # Setup folder structure
+rm -rf MidiEditor-win32
+
 mkdir MidiEditor-win32
 mkdir MidiEditor-win32/win_root/
 
 # Copy binary
-cp $MIDIEDITOR_BINARY_WINDOWS MidiEditor-win32/win_root/MidiEditor.exe
+cp -r $(dirname $MIDIEDITOR_BINARY_WINDOWS)/* MidiEditor-win32/win_root/
 
 # Copy metronome
-cp -R packaging/metronome MidiEditor-win32/win_root/metronome
+cp -R ../../packaging/metronome MidiEditor-win32/win_root/metronome
 
-cp -R packaging/windows/windows-installer/. MidiEditor-win32
+cp -R ../../packaging/windows/windows-installer/. MidiEditor-win32
 
 cd MidiEditor-win32
 
-sh ../$INSTALLJAMMER -DVersion $MIDIEDITOR_RELEASE_VERSION_STRING --build-for-release windows-installer.mpi
+sh "$INSTALLJAMMER" -DVersion $MIDIEDITOR_RELEASE_VERSION_STRING --build-for-release windows-installer.mpi
 
 cd ..
 
+rm -rf releases
 mkdir releases
 cp -a MidiEditor-win32/output/MidiEditor-$MIDIEDITOR_RELEASE_VERSION_STRING-Setup.exe releases/MidiEditor-$MIDIEDITOR_RELEASE_VERSION_STRING-Setup.exe
